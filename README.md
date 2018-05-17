c65ecd905189865eb97fa5268555205b38eb94a9
Gray Smith <gray.smith00@gmail.com>
2018-05-17 09:03:24 -0600

    Change html section to main, create Readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linked List - by Gray Smith & James Burbage
+
+A simple bookmark saving application so you will never forget to revisit that awesome website you found!
--- index.js
+++ index.js
@@ -172,6 +172,7 @@ function clearReadBookmarks() {
   bookmarkCounter();
   readBookmarksCounter();
   unreadBookmarksCounter();
+  displayClearBookmarksButton();
 }
 
 function displayClearBookmarksButton() {
